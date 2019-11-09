1ff0d49f5b064f853dd38fce19d0b58cf1c41759
kizx <kizxmoe@gmail.com>
2019-11-09 18:35:53 +0800

    简单捕获下异常

--- plugins/SteamPrice/__init__.py
+++ plugins/SteamPrice/__init__.py
@@ -1,2 +1,4 @@
+from plugins.SteamPrice.steam import *
+
 __plugin_name__ = 'steam史低查询'
 __plugin_usage__ = '史低 游戏id'
--- plugins/SteamPrice/steam.py
+++ plugins/SteamPrice/steam.py
@@ -7,9 +7,21 @@ import asyncio
 
 @on_command('steam', aliases=('史低',), only_to_me=False)
 async def price(session: CommandSession):
-    game_id = session.get('game', prompt='请输入游戏id')
+    game_id = session.get('game_id', prompt='请输入游戏id')
     msg = await get_price(game_id)
     await session.send(msg)
+
+
+@price.args_parser
+async def _(session: CommandSession):
+    stripped_arg = session.current_arg_text.strip()
+    if session.is_first_run:
+        if stripped_arg:
+            session.state['game_id'] = stripped_arg
+        return
+    if not stripped_arg:
+        session.pause('要查询的游戏id不能为空呢，请重新输入')
+    session.state[session.current_key] = stripped_arg
 
 
 async def get_price(game_id):
@@ -20,19 +32,24 @@ async def get_price(game_id):
     urllib3.disable_warnings()
     response = requests.get(url, headers=headers, verify=False)
 
-    root = etree.HTML(response.text)
-    path = "//table[@class='table table-bordered table-hover table-dark table-responsive-flex']//td"
-    name = root.xpath(path)[5].text
+    try:
+        root = etree.HTML(response.text)
+        path = "//table[@class='table table-bordered table-hover table-dark table-responsive-flex']//td"
+        name = root.xpath(path)[5].text
 
-    path = "//table[@class='table table-fixed table-prices table-hover table-sortable']//td[text()='Base Price']"
-    current_price = root.xpath(path + "/preceding-sibling::td/text()")[-1]
-    cheap_price = root.xpath(path + "/following-sibling::td/text()")[0]
-
-    return f"游戏名称：{name}\n当前价格：{current_price}\n史低价格：{cheap_price}"
+        path = "//table[@class='table table-fixed table-prices table-hover table-sortable']//td[text()='Base Price']"
+        current_price = root.xpath(path + "/preceding-sibling::td/text()")[-1]
+        cheap_price = root.xpath(path + "/following-sibling::td/text()")[0]
+    except IndexError:
+        return '输入的游戏id不对哟'
+    except:
+        return '发生未知错误'
+    else:
+        return f"游戏名称：{name}\n当前价格：{current_price}\n史低价格：{cheap_price}"
 
 
 if __name__ == '__main__':
     loop = asyncio.get_event_loop()
-    result = loop.run_until_complete(get_price(420060))
+    result = loop.run_until_complete(get_price(203160))
     print(result)
     loop.close()
